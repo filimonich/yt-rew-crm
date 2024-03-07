27c13fecc7504deed745af0fc8b7f66bb60f5895
Sergey Filimonov <srfilimonoff@gmail.com>
2024-03-07 15:56:49 +0300

    feat: add Sidebar

---
+++ layouts/Default.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+console.log("lodading layout");
+</script>
+<template>
+  <section class="grid" style="min-height: 100vh">
+    <LayoutSidebar />
+    <div>
+      <slot />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+}
+</style>

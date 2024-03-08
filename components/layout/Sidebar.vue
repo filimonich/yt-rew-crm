f07ae2f9cfeafc1d7d421e7dc721a37044d3ca07
Sergey Filimonov <srfilimonoff@gmail.com>
2024-03-08 22:13:44 +0300

    feat: add menu

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -3,11 +3,12 @@
 <template>
   <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
     <NuxtLink to="/" class="mb-10 block">
-      <NuxtImg src="/logo.svg" alt="logo" width="100px" class="mx-auto" />
+      <NuxtImg src="/logo.svg" alt="logo" width="140px" class="mx-auto" />
     </NuxtLink>
     <button class="absolute top-2 right-3 transition-colors hover:text-primary">
       <Icon name="line-md:log-out" size="20" />
     </button>
+    <LayoutMenu />
     <div></div>
   </aside>
 </template>

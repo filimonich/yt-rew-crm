5521763c1f3ce4339c9d55fb6ca1fc7c1ac9d49c
Sergey Filimonov <srfilimonoff@gmail.com>
2024-03-08 19:08:53 +0300

    feat: add google-fonts and styles for tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,21 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Lato: {
+            wght: [300, 400, 700],
+            ital: [300],
+          },
+        },
+      },
+    ],
+  ],
   shadcn: {
     prefix: "Ui",
     componentDir: "./components/ui",

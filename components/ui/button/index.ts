7c0f37dfdb2bfd310e850b7a98cf0b6eff464f96
Sergey Filimonov <srfilimonoff@gmail.com>
2024-03-09 16:31:14 +0300

    feat: add form login

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -14,7 +14,7 @@ export const buttonVariants = cva(
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-[#2E4157]",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
---
+++ pages/Login.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+useHead({
+  title: "Login | CRM System YT",
+});
+
+const emailRef = ref("");
+const passwordRef = ref("");
+const nameRef = ref("");
+
+watch(emailRef, () => {
+  console.log(emailRef.value);
+});
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen w-full">
+    <div class="rounded bg-sidebar w-1/4 p-5">
+      <h1 class="text-2xl font-bold text-center mb-5">Login</h1>
+
+      <form>
+        <UiInput
+          placeholder="Email"
+          type="email"
+          class="mb-3"
+          v-model="emailRef"
+        />
+        <UiInput
+          placeholder="Password"
+          type="password"
+          class="mb-3"
+          v-model="passwordRef"
+        />
+        <UiInput
+          placeholder="Name"
+          type="name"
+          class="mb-3"
+          v-model="nameRef"
+        />
+        <div class="flex items-center justify-center gap-5">
+          <UiButton type="button">Login</UiButton>
+          <UiButton type="button">Register</UiButton>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>

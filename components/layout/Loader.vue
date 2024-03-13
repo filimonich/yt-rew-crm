9c5ea2cca729a724becd8f06b2316213ec68f778
Sergey Filimonov <srfilimonoff@gmail.com>
2024-03-13 18:44:20 +0300

    feat: add login and register

--- components/layout/Loader.vue
+++ components/layout/Loader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-    <img src="/loader.svg" />
+    <NuxtImg src="/loader.svg" alt="loader" />
   </div>
 </template>
 
--- utils/appwrite.ts
+++ utils/appwrite.ts
@@ -1,9 +1,9 @@
+import { APP_WRITE_ID } from "@/app.constants";
 import { Account, Client, Databases, Storage } from "appwrite";
 
 export const client = new Client();
-const defaultValue = process.env.APP_WRITE_ID as string;
 
-client.setEndpoint("https://cloud.appwrite.io/v1").setProject(defaultValue);
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject(APP_WRITE_ID);
 
 export const account = new Account(client);
 export { ID } from "appwrite";
